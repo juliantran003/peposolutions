b0e01a5d8d4347a1e215c501a942a470f9c5c061
Julian Tran <juliantran003@gmail.com>
2025-03-11 14:21:35 +0000

    new desktop footer

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,35 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div>
+      <div className="footer1">
+        <div>
+          <Image src="/img/logos/W&B-logo.png" fill alt="Pepo Solutions" />
+        </div>
+        <p>
+          Empowering small food businesses to reach the next step on their
+          journey
+        </p>
+      </div>
+      <div className="footer2">
+        <div>
+          <Image src="/img/instagram.png" fill alt="Pepo Solutions" />
+        </div>
+        <p>Follow our work on Instagram</p>
+      </div>
+      <div className="footer3">
+        <h3>NAVIGATION</h3>
+        <ul>
+          <Link href="/">Home</Link>
+          <Link href="/services">Services</Link>
+          <Link href="/portfolio">Portfolio</Link>
+          <Link href="/about">About</Link>
+        </ul>
+      </div>
+      <div className="footer4">
+        <h3>CONTACT</h3>
+        <a href="mailto:someone@yoursite.com">pepo@peposolutions.com</a>
+      </div>
+      {/* <div>
         <Image src="/img/instagram.png" fill alt="Instagram" />
       </div>
       <div>
@@ -13,7 +41,7 @@ export default function Footer() {
       </div>
       <Link href="email:to" className="button_1">
         Contact Us
-      </Link>
+      </Link> */}
     </footer>
   );
 }
